561370215926934dc204aded757cc22e090e144d
Vlad Bibire <vlad@3sidedcube.com>
2024-10-11 13:06:14 +0300

    Single button

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,10 @@ import { IoCard } from 'react-icons/io5';
 
 const App = () => {
   return (
-    <div className="h-screen w-screen bg-black py-8 text-center">
-      <div className="flex flex-col items-center justify-center gap-8">
-        <Button color="silver" icon={<IoCard size={24} />}>
-          1-Click Buy
-        </Button>
-        <Button color="gold">1-Click Buy</Button>
-        <Button color="green">1-Click Buy</Button>
-        <Button color="magenta">1-Click Buy</Button>
-        <Button color="red">1-Click Buy</Button>
-      </div>
+    <div className="flex h-screen w-screen items-center justify-center bg-black py-8">
+      <Button color="red" icon={<IoCard size={24} />}>
+        1-Click Buy
+      </Button>
     </div>
   );
 };
